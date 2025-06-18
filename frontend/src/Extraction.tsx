7f67c996445bd244ac74906b8423876ab7021fbd
haru <hinagoro5656@gmail.com>
2025-06-18 16:58:54 +0900

    fix: デザインを修正

--- frontend/src/Extraction.tsx
+++ frontend/src/Extraction.tsx
@@ -13,15 +13,15 @@ import { Sparkles } from "lucide-react";
 
 function Extraction() {
   return (
-    <div className="h-screen  flex justify-center items-center  flex-col bg-neutral-50">
-      <div className="text-left max-w-2xl w-full px-6">
-        <h1 className="text-3xl pb-8 font-bold ">議事録からタスクを自動抽出</h1>
-        <div className=" bg-white p-8 rounded-lg shadow-md ">
+    <div className="grow flex justify-center items-center flex-col bg-neutral-100">
+      <div className="text-left max-w-4xl w-full px-6">
+        <h1 className="text-3xl pb-4 font-bold ">議事録からタスクを自動抽出</h1>
+        <div className=" bg-white md:p-8 p-6 rounded-xl border border-border">
           <div>
             議事録を貼り付けまたはテキストファイルをアップロードしてください。
           </div>
           <Textarea
-            className="w-full mt-4 mb-8 h-44 resize-none p-3"
+            className="w-full mt-3 mb-6 h-44 resize-x-none p-3"
             placeholder="例:
            ・プロジェクトAの進捗確認を田中さんが来週月曜までに実施"
           ></Textarea>
@@ -48,7 +48,7 @@ function Extraction() {
             </DropzoneZone>
           </Dropzone>
 
-          <Button className="w-full py-5 text-center">
+          <Button className="w-full font-bold py-4 text-center" size={"lg"}>
             <Sparkles />
             タスクを抽出する
           </Button>
