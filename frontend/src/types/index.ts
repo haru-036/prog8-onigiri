44c2a74245045dc69fc9bab8702576840d955780
haru <hinagoro5656@gmail.com>
2025-06-21 20:23:29 +0900

    feat: タスクカードにグループIDを追加し、ユーザー名の表示を改善

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -5,6 +5,11 @@ export type Task = {
   deadline: Date | string; // ISO date string or Date object
   priority: "low" | "middle" | "high";
   assign?: number; // User ID or name
+  assigned_user?: {
+    id: number;
+    user_name: string;
+    picture: string;
+  };
   group_id?: number; // Group ID
   status: "not-started-yet" | "in-progress" | "done";
   createdAt?: Date | string;
