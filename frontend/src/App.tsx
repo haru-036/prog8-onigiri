45899fa8fbd1da7138fced4fd41d5226088b4e39
haru <hinagoro5656@gmail.com>
2025-06-22 15:53:28 +0900

    feat: OGP設定を追加し、アプリのタイトルと説明を設定

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import { FileText } from "lucide-react";
 import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div className="min-h-svh flex justify-center items-center flex-col">
       <div className="text-left">
+        <div className="flex items-center gap-2 pb-6">
+          <span className="bg-linear-to-tr from-purple-700 to-primary rounded-md size-9 grid place-content-center">
+            <FileText className="size-5 text-white" />
+          </span>
+          <div className="font-bold text-xl">MeeTask</div>
+        </div>
         <div className="text-2xl pb-8 font-bold ">
           ミーティング後、 <br />
           議事録からタスクを自動追加。
